0f38bcf3d8476033fd4f5af775b6a36c870d93a1
Al-Amin Hossain <prodeveloperalamin@gmail.com>
2025-07-25 04:45:07 +0600

    code update

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+  function handleClick(){
+    alert('Button Clicked Done Boss')
+  };
+
+  const handleClick2 = () => {
+    alert("wrong man")
+  }
+
+  const addToFive = (num) => {
+    alert(num + 6)
+  }
+
+  return (
+    <>
+     <div>
+      <h1>React Core conceft</h1>
+      <button onClick={handleClick}>Click Here Now</button>
+      <button onClick={handleClick2}>Here Now</button>
+      <button onClick={() => alert(('clicked 3rd btn'))} >btn 3</button>
+      <button onClick={()=> addToFive(3) } >Button 4</button>
+    
+      </div>
+    </>
+  )
+}
+
+export default App
